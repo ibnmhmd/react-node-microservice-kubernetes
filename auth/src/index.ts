14c6f5d865d3978a9f302fed8b467556ac1470db
Amine Mohammad <34374600+ibnmhmd@users.noreply.github.com>
2025-06-21 16:25:00 +0400

    console log

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import { app , PORT } from './app';
 import { DatabaseConnectionError } from '@ticko/common';
 const start = async () => {
   try {
+    console.log('starting auth server')
     if(!process.env.JWT_KEY) {
       throw new Error('JWT_KEY must be defined');
     }
